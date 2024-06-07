6612167e8e2f728538934abccd2c683ec1f1887f
bloch navaj <navajbloch420@gmail.com>
2024-06-07 21:59:00 +0530

    new code

--- String/S1.java
+++ String/S1.java
@@ -4,10 +4,6 @@ import java.util.*;
 
 public class S1 {
   public static void main(String[] args) {
-    String name = "bloch";
-    String name2 = "navaj";
-    String fullName = name + " " + name2;
-    System.out.println(fullName);
 
   }
 
--- main.java
+++ main.java
@@ -2,6 +2,30 @@ import java.util.*;
 
 public class main {
         public static void main(String[] args) {
+                StringBuilder sb = new StringBuilder("navaj");
+                System.out.println(sb);
+
+                // print character
+                // System.out.println(sb.charAt(4));
+
+                // set charater at index
+                // sb.setCharAt(0, 'p');
+                // System.out.println(sb);
+
+                // // insert in stringbuilder
+                // sb.insert(2, 's');
+                // System.out.println(sb);
+
+                // // delete the extra "s"
+                // sb.delete(2, 4);
+                // System.out.println("delete word new string is : " + sb);
+
+                sb.append("b");
+                sb.append("l");
+                sb.append("o");
+                sb.append("c");
+                sb.append("h");
+                System.out.println(sb);
 
         }
 
