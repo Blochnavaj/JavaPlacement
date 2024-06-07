4d216bc77dc22c02ac7e441b90961cc9bc44e89d
bloch navaj <navajbloch420@gmail.com>
2024-06-07 21:59:35 +0530

    new

--- String/S1.java
+++ String/S1.java
@@ -4,6 +4,10 @@ import java.util.*;
 
 public class S1 {
   public static void main(String[] args) {
+    String name = "bloch";
+    String name2 = "navaj";
+    String fullName = name + " " + name2;
+    System.out.println(fullName);
 
   }
 
